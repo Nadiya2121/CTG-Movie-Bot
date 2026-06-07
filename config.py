eb3b6acdd7b545495f5ac8b4f330455b496d5817
Nadiya2121 <misshadiya21@gmail.com>
2026-06-07 11:05:52 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,18 +4,18 @@ API_ID = 29462738          # আপনার Telegram API ID (integer)
 API_HASH = "297f51aaab99720a09e80273628c3c24"  # আপনার Telegram API Hash (string)
 BOT_TOKEN = "8970571544:AAHkBPmDzF9crRDbeCq6-Z3RNqDpEe7i9QA"  # BotFather থেকে পাওয়া টোকেন
 ADMIN_ID = 8297458824      # আপনার নিজের টেলিগ্রাম ইউজার আইডি
-MAIN_CHANNEL_ID = -1003973741130  # আপনার প্রধান চ্যানেলের আইডি
+MAIN_CHANNEL_ID = -1003973741130  # প্রধান মুভি চ্যানেল আইডি (ফোর্স সাবস্ক্রিপশন)
 
 # --- ডাবল ডাটাবেজ কনফিগারেশন ---
-DATABASE_URI = "mongodb+srv://hahema9427:hahema9427@cluster0.3mf49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_URI = "mongodb+srv://hahema9427:hahema9427@cluster0.3mf49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # আপনার ১ম MongoDB লিঙ্ক
 MULTIPLE_DB = False                 
 DATABASE_URI2 = "mongodb+srv://..." 
 
 # --- আর্নিং ও মিনি অ্যাপ কনফিগারেশন ---
 BOT_USERNAME = "CTGMovieBot"  # আপনার বটের ইউজারনেম ( শুরুতে @ দেবেন না)
-WEB_URL = "gorgeous-donetta-nahidcrk-7b84dba9.koyeb.app/view/CTG-Movie-Bot"  # Render থেকে পাওয়া আপনার অ্যাপের লিংক ( শুরুতে https:// দেবেন না)
+WEB_URL = "gorgeous-donetta-nahidcrk-7b84dba9.koyeb.app/view/CTG-Movie-Bot"  # Koyeb অ্যাপের লিংক ( শুরুতে https:// দেবেন না)
 
-# আপনার আয়ের ডিরেক্ট অ্যাড লিংক (এখানে Adsterra বা অন্য কোম্পানির ডিরেক্ট অ্যাড লিংক দিন)
+# আপনার বিজ্ঞপ্তির ডিরেক্ট লিংক (Adsterra-র আসল লিংক বসানো হয়েছে)
 DIRECT_AD_LINKS = [
     "https://www.effectivecpmnetwork.com/m30pd1efbm?key=e7b1df640000086bd44393a929d6ebab",
     "https://www.effectivecpmnetwork.com/jxuaz5zk?key=6faf197d33f98bcb3bc070b020c19664"
@@ -24,3 +24,7 @@ DIRECT_AD_LINKS = [
 # --- আপনার নিজস্ব প্রমোশনাল চ্যানেল লিংকসমূহ ---
 CHANNEL_LINK_1 = "https://t.me/+6hvCoblt6CxhZjhl"  # All Movie Link
 CHANNEL_LINK_2 = "https://t.me/TGLinkBase"         # Backup Channel
+
+# --- নতুন ভিজ্যুয়াল ডিজাইন কনফিগারেশন ---
+# স্টার্ট কমান্ড দিলে যে ব্যানার বা অ্যানিমেটেড GIF দেখাবে (এখানে যেকোনো ডিরেক্ট ইমেজ বা GIF লিংক বসাতে পারবেন)
+START_BANNER = "https://github.com/NBBotz/Images/blob/main/Lucia-Filter-Bot.jpeg?raw=true"
